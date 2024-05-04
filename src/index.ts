c90229dd53a34be890b44d30dfb9fd4d56128d09
herbertdesousa <sousaherbert138@gmail.com>
2024-05-04 14:15:13 -0300

    FEAT: select drawer

--- src/index.ts
+++ src/index.ts
@@ -29,18 +29,52 @@ process.stdin.setEncoding("utf8");
 
 const select = new Select(["opt 1", "opt 2", "opt 3"]);
 
+interface SelectPrintStyle {
+  paddingLeft?: number;
+  paddingRight?: number;
+}
+
+function generateSpaces(amount: number): string {
+  return " ".repeat(amount);
+}
+
+class SelectDrawer {
+  private style: Required<SelectPrintStyle>;
+
+  constructor(private select: Select, style: SelectPrintStyle) {
+    this.style = { paddingLeft: 0, paddingRight: 0, ...style };
+  }
+
+  draw(): string {
+    const selectedOption = this.select.get();
+    const leftSpaces = generateSpaces(this.style.paddingLeft);
+    const rightSpaces = generateSpaces(this.style.paddingRight);
+
+    const lines: string[] = [];
+
+    for (const option of this.select.all()) {
+      const line = `${leftSpaces}${option}${rightSpaces}`;
+
+      if (option === selectedOption) {
+        lines.push(chalk.bgGreenBright.black(line));
+      } else {
+        lines.push(line);
+      }
+    }
+
+    return lines.join("\n");
+  }
+}
+
+const selectPrinter = new SelectDrawer(select, {
+  paddingLeft: 2,
+  paddingRight: 5,
+});
+
 function render() {
   console.clear();
 
-  const selectedOption = select.get();
-
-  for (const option of select.all()) {
-    if (option === selectedOption) {
-      console.log(chalk.bgBlue(`    ${option}    `));
-    } else {
-      console.log(`    ${option}    `);
-    }
-  }
+  console.log(selectPrinter.draw());
 }
 
 render();
