5c6fe7072c96498460fa5487fe74ccfc9f06a224
herbert de sousa <sousaherbert138@gmail.com>
2024-05-04 21:29:26 -0300

    FEAT: select drawer cropping text

--- src/SelectDrawer.spec.ts
+++ src/SelectDrawer.spec.ts
@@ -4,7 +4,7 @@ import { SelectDrawer } from "./SelectDrawer";
 
 describe("SelectDrawer", () => {
   it("should be able to draw simple", () => {
-    const select = new Select(["opt 1", "opt 2", "opt 3"]);
+    const select = new Select(["opt 1", "opt two", "opt 3"]);
     const drawer = new Drawer();
 
     jest.spyOn(select, "get").mockImplementationOnce(() => "opt 1");
@@ -23,136 +23,8 @@ describe("SelectDrawer", () => {
 
     expect(selectPrinter.draw()).toEqual([
       selectedLine,
-      " opt 2    ",
+      " opt two  ",
       " opt 3    ",
     ]);
   });
-
-  it("should be able to fit different text widths", () => {
-    const select = new Select(["opt 1", "opt prime", "opt 3"]);
-    const drawer = new Drawer();
-
-    jest.spyOn(select, "get").mockImplementationOnce(() => "opt 1");
-
-    const selectPrinter = new SelectDrawer(select, drawer, {
-      paddingLeft: 1,
-      paddingRight: 1,
-      width: 10,
-    });
-
-    const selectedLine = drawer.compose({
-      text: " opt 1    ",
-      applyIn: "bg",
-      color: "primary",
-    });
-
-    expect(selectPrinter.draw()).toEqual([
-      selectedLine,
-      " opt prime",
-      " opt 3    ",
-    ]);
-  });
-
-  it("should be able to crop if width is too small", () => {
-    const select = new Select(["opt 1", "opt prime", "opt 3"]);
-    const drawer = new Drawer();
-
-    jest.spyOn(select, "get").mockImplementation(() => "opt 1");
-
-    const selectPrinter1 = new SelectDrawer(select, drawer, { width: 5 });
-
-    const selectedLine1 = drawer.compose({
-      text: "opt 1",
-      applyIn: "bg",
-      color: "primary",
-    });
-
-    expect(selectPrinter1.draw()).toEqual([selectedLine1, "op...", "opt 3"]);
-
-    //
-
-    const selectPrinter2 = new SelectDrawer(select, drawer, {
-      paddingLeft: 2,
-      paddingRight: 2,
-      width: 5,
-    });
-
-    const selectedLine2 = drawer.compose({
-      text: "  ...",
-      applyIn: "bg",
-      color: "primary",
-    });
-
-    expect(selectPrinter2.draw()).toEqual([selectedLine2, "  ...", "  ..."]);
-
-    //
-
-    const selectPrinter3 = new SelectDrawer(select, drawer, {
-      paddingLeft: 2,
-      paddingRight: 2,
-      width: 3,
-    });
-
-    const selectedLine3 = drawer.compose({
-      text: "  .",
-      applyIn: "bg",
-      color: "primary",
-    });
-
-    expect(selectPrinter3.draw()).toEqual([selectedLine3, "  .", "  ."]);
-
-    //
-
-    const selectPrinter4 = new SelectDrawer(select, drawer, {
-      paddingLeft: 2,
-      paddingRight: 2,
-      width: 2,
-    });
-
-    const selectedLine4 = drawer.compose({
-      text: "  ",
-      applyIn: "bg",
-      color: "primary",
-    });
-
-    expect(selectPrinter4.draw()).toEqual([selectedLine4, "  ", "  "]);
-
-    //
-
-    const selectPrinter5 = new SelectDrawer(select, drawer, {
-      paddingLeft: 2,
-      paddingRight: 2,
-      width: 1,
-    });
-
-    const selectedLine5 = drawer.compose({
-      text: " ",
-      applyIn: "bg",
-      color: "primary",
-    });
-
-    expect(selectPrinter5.draw()).toEqual([selectedLine5, " ", " "]);
-  });
-
-  /* it('should be able to draw with same padding end with different widths', () => {
-    const select = new Select(["opt 1", "opt prime", "opt"]);
-    const drawer = new Drawer();
-
-    jest.spyOn(select, "get").mockImplementationOnce(() => "opt 1");
-
-    const selectPrinter2 = new SelectDrawer(select, drawer, {
-      paddingRight: 5,
-    });
-
-    "opt 1         "
-    "opt prime     "
-
-    expect(selectPrinter2.draw()).toBe(
-      drawer.compose({
-        text: "opt 1     ",
-        color: "primary",
-        applyIn: "bg",
-      }) + "\nopt prime     \nopt 3",
-    );
-  }); */
 });
--- src/SelectDrawer.ts
+++ src/SelectDrawer.ts
@@ -1,8 +1,6 @@
-import chalk from "chalk";
-import { generateSpaces } from "./generateSpaces";
-
-import { Select } from "./Select";
 import { Drawer } from "./Drawer";
+import { Select } from "./Select";
+import { cropText } from "./cropText";
 
 interface SelectPrintStyle {
   paddingLeft?: number;
@@ -27,42 +25,13 @@ export class SelectDrawer {
     const selectedOption = this.select.get();
 
     const lines = this.select.all().map((option) => {
-      let line = option;
-
-      // width:   x   (1 length)
-      // padding: x x (2 length)
-      // result:  x   (1 length)
-      if (this.style.paddingLeft > this.style.width) {
-        return generateSpaces(this.style.width);
-      }
-
-      line = generateSpaces(this.style.paddingLeft) + line;
-
-      line = line.padEnd(this.style.width, " ");
-
-      // width: a b c X X (3 length)
-      // line:  a b c d e (5 length)
-      // result: 2
-      const diffLineWidth = line.length - this.style.width;
-
-      const isCrossingWidth = diffLineWidth > 0;
-
-      if (isCrossingWidth) {
-        const moreSymbolMaxWidth = this.style.width - this.style.paddingLeft;
-
-        // ... -> width = 1 -> .
-        // ... -> width = 2 -> ..
-        // ... -> width = 5 -> ...
-        const currentMoreSymbol = this.moreSymbol.slice(0, moreSymbolMaxWidth);
-
-        // more symbol: ...  (3 length) -> adjusted symbol = .. (2 length)
-        // width:  a b c     (3 length)
-        // line:   a b c d e (5 length)
-        // result: a b c . . (5 length)
-        line = line.slice(0, this.style.width - currentMoreSymbol.length);
-
-        line += currentMoreSymbol;
-      }
+      const line = cropText({
+        text: option,
+        moreSymbol: this.moreSymbol,
+        paddingLeft: this.style.paddingLeft,
+        paddingRight: this.style.paddingRight,
+        width: this.style.width,
+      });
 
       if (option === selectedOption) {
         return this.drawer.compose({
