e60b51e34033c116e935abbf3c3a70899879f2c4
herbert de sousa <sousaherbert138@gmail.com>
2024-05-04 21:43:28 -0300

    LINT

--- src/Drawer.ts
+++ src/Drawer.ts
@@ -1,33 +1,17 @@
+import chalk from "chalk";
+
 type DrawerColor = "primary";
 
 type DrawerComposeReq = {
   text: string;
   color: DrawerColor;
-  applyIn: 'bg' | 'text';
+  applyIn: "bg" | "text";
 };
 
 type ComposeResolver = Record<
   DrawerComposeReq["color"],
   Record<DrawerComposeReq["applyIn"], (value: string) => string>
 >;
-
-/*
-function helperChalk(fn: any) {
-  const [left, right] = String(fn('hello')).split('hello');
-
-  console.log({
-    text: '(txt) => `' + left + '${txt}' + right + '`'
-  })
-}
-
-helperChalk(chalk.blue)
-
-console.log(new Drawer().compose({
-  text: 'hello',
-  color: 'primary',
-  applyIn: 'text'
-}))
-*/
 
 export class Drawer {
   compose(payload: DrawerComposeReq): string {
@@ -36,9 +20,9 @@ export class Drawer {
         bg: (txt) => `\x1B[44m${txt}\x1B[49m`,
         text: (txt) => `\x1B[34m${txt}\x1B[39m`,
       },
-    }
+    };
 
-    const resolve = resolver[payload.color][payload.applyIn]
+    const resolve = resolver[payload.color][payload.applyIn];
 
     if (!resolve) return payload.text;
 
